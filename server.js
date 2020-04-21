993a7f3f4ab7e2c9f9988def509e257e99d267fa
unknown <aleksmilenkov@yahoo.com>
2020-04-21 15:49:29 +0200

    add user model and connect to db

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
+const User=require('./models/User');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost/userData')
 
 app.use(bodyParser.json());
 
